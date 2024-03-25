af370805afb7206863398d62a15dc80de7e4eb3e
nunhes <nunhes@gmail.com>
2024-03-25 18:02:00 +0100

    revisión 1

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,51 @@
-/* import '../css/style.css'
+// selecciona o contedor de palabras no documento HTML.
+const wrapper = document.querySelector(".words");
 
-import viteLogo from '/img/vite.svg'
+// selecciona tódalas palabras individuais dentro do contedor.
+const words = wrapper.querySelectorAll("span");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
+// selecciona a palabra actual dentro d contedor.
+const currentWord = wrapper.querySelector("span.current");
 
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// obtén o ancho de cada palabra e almacenao nun array.
+const wordsWidths = Array.from(words).map((word) => word.offsetWidth);
 
- */
+// atopa o ancho máximo entre tódalas palabras.
+const maxWordsWidth = Math.max(...wordsWidths);
+
+// define as clases para a palabra actual e a seguinte.
+const CURRENT_CLASS = "current";
+const NEXT_CLASS = "next";
+
+// establece a propiedade CSS '--width' co ancho da palabra actual para o contedor.
+wrapper.style.setProperty("--width", `${currentWord.offsetWidth}px`);
+
+// establece a propiedade CSS '--width-mobile' co ancho máximo de todalas palabras para o contedor.
+wrapper.style.setProperty("--width-mobile", `${maxWordsWidth}px`);
+
+// executa a función cada 1500 milisegundos (1.5 segundos).
+setInterval(() => {
+  // atopa a palabra actual e a seguinte dentro do contedor.
+  const currentWord = wrapper.querySelector("span.current");
+  const nextWord = wrapper.querySelector("span.next");
+
+  // atopa a palabra seguinte á seguinte (si existe, do contrario, volve á primeira palabra).
+  const nextNextWord = nextWord.nextElementSibling
+    ? nextWord.nextElementSibling
+    : wrapper.firstElementChild;
+
+  // quita as clases 'current' e 'next' da palabra actual e a seguinte, respectivamente.
+  currentWord.classList.remove(CURRENT_CLASS);
+  nextWord.classList.remove(NEXT_CLASS);
+
+  // agrega a clase 'current' á seguinte palabra e 'next' á palabra seguinte á seguinte.
+  nextWord.classList.add(CURRENT_CLASS);
+  nextNextWord.classList.add(NEXT_CLASS);
+
+  // establece as propiedades CSS '--color' e '--color-bg' cos valores de color de datos da seguinte palabra.
+  wrapper.style.setProperty("--color", nextWord.dataset.color);
+  wrapper.style.setProperty("--color-bg", nextWord.dataset.bgColor);
+
+  // establece a propiedade CSS '--width' co ancho da seguinte palabra para o contedor.
+  wrapper.style.setProperty("--width", `${nextWord.offsetWidth}px`);
+}, 1500);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+// import '@fontsource-variable/open-sans';
+
 export default {
 
   content: ["./src/**/*.{html,js}"],
 
   theme: {
     extend: {
+      fontFamily: {
+        customFont: ['Montserrat', 'sans-serif'],
+        // Add more custom font families as needed
+      },
+
+      colors: {
+        "primeiro": "hsl(50, 100%, 50%)",
+        "segundo": "hsl(228, 39%, 23%)",
+        "terceiro": "hsl(227, 12%, 61%)",
+      },
 
     },
   },
